962d6da1126074a77c136068fa7d86c3fe5cda63
KurtMDasal <kurtdasal@gmail.com>
2020-03-24 23:31:35 +0800

    6WCSERVER

---
+++ src/components/Navbar.vue
@@ -0,0 +1,43 @@
+<template>
+<section class="menu cid-qxyOwCSYXH" once="menu" id="menu2-1" data-rv-view="74">
+<nav class="navbar navbar-expand beta-menu navbar-dropdown align-items-center navbar-fixed-top navbar-toggleable-sm bg-color transparent">
+        <button class="navbar-toggler navbar-toggler-right" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+            <div class="hamburger">
+                <span></span>
+                <span></span>
+                <span></span>
+                <span></span>
+            </div>
+        </button>
+        <div class="menu-logo">
+            <div class="navbar-brand">
+                
+                <span class="navbar-caption-wrap"><a class="navbar-caption text-white display-4" href="#">
+                        KD EDUCATION
+                    </a></span>
+            </div>
+        </div>
+        <div class="collapse navbar-collapse" id="navbarSupportedContent">
+            <ul class="navbar-nav nav-dropdown" data-app-modern-menu="true"><li class="nav-item">
+                    <a class="nav-link link text-white display-4" href="/">
+                        Home
+                    </a>
+                </li>
+                <li class="nav-item">
+                    <a class="nav-link link text-white display-4" href="/News">
+                        News
+                    </a>
+                </li>
+               <li class="nav-item">
+                    <a class="nav-link link text-white display-4" href="/Members">
+                        Members
+                    </a>
+                </li></ul>
+                
+            <div class="navbar-buttons mbr-section-btn"><a class="btn btn-sm btn-white display-4" href="tel:+1-234-567-8901">
+                        Search...
+                    </a></div>          
+        </div>
+    </nav>
+    </section>
+    </template>
---
+++ src/views/Members.vue
@@ -0,0 +1,139 @@
+<template>
+<section class="testimonials4 cid-qxz0gNvgdF" id="testimonials4-8" data-rv-view="87">
+  
+  <div class="container">
+    <h2 class="pb-3 mbr-fonts-style mbr-white align-center display-2"><strong>Members</strong></h2>
+    <div class="col-md-10 testimonials-container"> 
+      
+
+      
+    <div class="testimonials-item">
+        <div class="user row">
+          <div class="col-lg-3 col-md-4">
+            <div class="user_image">
+              <img src="../assets/6767-960x960.jpg" alt="" title="" media-simple="true">
+            </div>
+          </div>
+          <div class="testimonials-caption col-lg-9 col-md-8">
+            <div class="user_text ">
+              <p class="mbr-fonts-style  display-7">
+                 Lorem ipsum dolor sit amet, consectetur adipisicing elit. Vitae nostrum, quos voluptas fugiat blanditiis, temporibus expedita cumque doloribus ea, officiis consequuntur repellat minus ad veritatis? Facere similique accusamus, accusantium sunt!
+              </p>
+            </div>
+            <div class="user_name mbr-bold mbr-fonts-style align-left pt-3 display-7">
+                 Jon <button class="button" id="button1" style="margin-right:10px; float:right; background-color: black; color:white; border: none; padding: 5px 20px;">View</button> <button class="button" id="button1" style="float:right; background-color: black;color:white; border: none; padding: 5px 20px; margin-right: 10px;">Add</button>
+            </div>
+            <div class="user_desk mbr-light mbr-fonts-style align-left pt-2 display-7">
+                 English Professor</div>
+          </div>
+        </div>
+      </div><div class="testimonials-item">
+        <div class="user row">
+          <div class="col-lg-3 col-md-4">
+            <div class="user_image">
+              <img src="../assets/22-960x960.jpg" alt="" title="" media-simple="true">
+            </div>
+          </div>
+          <div class="testimonials-caption col-lg-9 col-md-8">
+            <div class="user_text">
+              <p class="mbr-fonts-style  display-7">
+                 Lorem ipsum dolor sit amet, consectetur adipisicing elit. Vitae nostrum, quos voluptas fugiat blanditiis, temporibus expedita cumque doloribus ea, officiis consequuntur repellat minus ad veritatis? Facere similique accusamus, accusantium sunt!
+              </p>
+            </div>
+            <div class="user_name mbr-bold mbr-fonts-style align-left pt-3 display-7">
+                 Regina<button class="button" id="button1" style="margin-right:10px; float:right; background-color: black; color:white; border: none; padding: 5px 20px;">View</button> <button class="button" id="button1" style="float:right; background-color: black;color:white; border: none; padding: 5px 20px; margin-right: 10px;">Add</button>
+            </div>
+            <div class="user_desk mbr-light mbr-fonts-style align-left pt-2 display-7">
+                 Mathematics Professor</div>
+          </div>
+        </div>
+      </div><div class="testimonials-item">
+        <div class="user row">
+          <div class="col-lg-3 col-md-4">
+            <div class="user_image">
+              <img src="../assets/trte-960x960.jpg" alt="" title="" media-simple="true">
+            </div>
+          </div>
+          <div class="testimonials-caption col-lg-9 col-md-8">
+            <div class="user_text">
+              <p class="mbr-fonts-style  display-7">
+                 Lorem ipsum dolor sit amet, consectetur adipisicing elit. Vitae nostrum, quos voluptas fugiat blanditiis, temporibus expedita cumque doloribus ea, officiis consequuntur repellat minus ad veritatis? Facere similique accusamus, accusantium sunt!
+              </p>
+            </div>
+            <div class="user_name mbr-bold mbr-fonts-style align-left pt-3 display-7">
+                 Tom <button class="button" id="button1" style="margin-right:10px; float:right; background-color: black; color:white; border: none; padding: 5px 20px;">View</button> <button class="button" id="button1" style="float:right; background-color: black;color:white; border: none; padding: 5px 20px; margin-right: 10px;">Add</button>
+            </div>
+            <div class="user_desk mbr-light mbr-fonts-style align-left pt-2 display-7">
+                 Guidance Counselor</div>
+          </div>
+        </div>
+      </div><div class="testimonials-item">
+        <div class="user row">
+          <div class="col-lg-3 col-md-4">
+            <div class="user_image">
+              <img src="../assets/3434-960x960.jpg" alt="" title="" media-simple="true">
+            </div>
+          </div>
+          <div class="testimonials-caption col-lg-9 col-md-8">
+            <div class="user_text">
+              <p class="mbr-fonts-style  display-7">
+                 Lorem ipsum dolor sit amet, consectetur adipisicing elit. Vitae nostrum, quos voluptas fugiat blanditiis, temporibus expedita cumque doloribus ea, officiis consequuntur repellat minus ad veritatis? Facere similique accusamus, accusantium sunt!
+              </p>
+            </div>
+            <div class="user_name mbr-bold mbr-fonts-style align-left pt-3 display-7">
+                 Arnold <button class="button" id="button1" style="margin-right:10px; float:right; background-color: black; color:white; border: none; padding: 5px 20px;">View</button> <button class="button" id="button1" style="float:right; background-color: black;color:white; border: none; padding: 5px 20px; margin-right: 10px;">Add</button>
+            </div>
+            <div class="user_desk mbr-light mbr-fonts-style align-left pt-2 display-7">
+                 Science Professor</div>
+          </div>
+
+        </div>
+      </div>
+      <div class="testimonials-item">
+        <div class="user row">
+          <div class="col-lg-3 col-md-4">
+            <div class="user_image">
+              <img src="../assets/img1.jpg" alt="" title="" media-simple="true">
+            </div>
+          </div>
+          <div class="testimonials-caption col-lg-9 col-md-8">
+            <div class="user_text">
+              <p class="mbr-fonts-style  display-7">
+                 Lorem ipsum dolor sit amet, consectetur adipisicing elit. Vitae nostrum, quos voluptas fugiat blanditiis, temporibus expedita cumque doloribus ea, officiis consequuntur repellat minus ad veritatis? Facere similique accusamus, accusantium sunt!
+              </p>
+            </div>
+            <div class="user_name mbr-bold mbr-fonts-style align-left pt-3 display-7">
+                 Anne <button class="button" id="button1" style="margin-right:10px; float:right; background-color: black; color:white; border: none; padding: 5px 20px;">View</button> <button class="button" id="button1" style="float:right; background-color: black;color:white; border: none; padding: 5px 20px; margin-right: 10px;">Add</button>
+            </div>
+            <div class="user_desk mbr-light mbr-fonts-style align-left pt-2 display-7">
+                 President</div>
+          </div>
+          
+        </div>
+      </div>
+      <div class="testimonials-item">
+        <div class="user row">
+          <div class="col-lg-3 col-md-4">
+            <div class="user_image">
+              <img src="../assets/img2.jpg" alt="" title="" media-simple="true">
+            </div>
+          </div>
+          <div class="testimonials-caption col-lg-9 col-md-8">
+            <div class="user_text">
+              <p class="mbr-fonts-style  display-7">
+                 Lorem ipsum dolor sit amet, consectetur adipisicing elit. Vitae nostrum, quos voluptas fugiat blanditiis, temporibus expedita cumque doloribus ea, officiis consequuntur repellat minus ad veritatis? Facere similique accusamus, accusantium sunt!
+              </p>
+            </div>
+            <div class="user_name mbr-bold mbr-fonts-style align-left pt-3 display-7">
+                 Robert <button class="button" id="button1" style="margin-right:10px; float:right; background-color: black; color:white; border: none; padding: 5px 20px;">View</button> <button class="button" id="button1" style="float:right; background-color: black;color:white; border: none; padding: 5px 20px; margin-right: 10px;">Add</button>
+            </div>
+            <div class="user_desk mbr-light mbr-fonts-style align-left pt-2 display-7">
+                 Biology Professor</div>
+          </div>
+          
+        </div>
+      </div>
+    </div>
+  </div>
+</section>
+</template>
